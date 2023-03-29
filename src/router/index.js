e73057876563fd47fe735ed8c412aed3263ed39a
xin1126 <z26319491z@gmail.com>
2023-03-30 00:24:48 +0800

    fix: router LayoutView

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ const routes = [
   {
     path: '/',
     name: 'layout',
-    component: () => import('@/pages/layoutView.vue'),
+    component: () => import('@/pages/LayoutView.vue'),
     redirect: '/pagination',
     children: [
       {
